40c633f5ba2699930c64e4e6788fb3005672a01d
liaozhongjun <liaozhongjun@droi.com>
2017-05-08 17:23:49 +0800

    delete useless code

--- app/src/main/java/com/liao/viewfunny/view/recyclerview/AlphabetItem.java
+++ app/src/main/java/com/liao/viewfunny/view/recyclerview/AlphabetItem.java
@@ -17,26 +17,6 @@ public class AlphabetItem {
             String s1 = o1.mKey;
             String s2 = o2.mKey;
             return s1.compareToIgnoreCase(s2);
-
-//            for (int i = 0; i < s1.length(); i++) {
-//                if (s2.length() <= i) {
-//                    return 1;
-//                }
-//                s1.compareToIgnoreCase(s2);
-//
-//                if (s1.charAt(i) < s2.charAt(i)) {
-//                    return -1;
-//                } else if (s1.charAt(i) > s2.charAt(i)) {
-//                    return 1;
-//                }
-//            }
-//            if (s1.length() == s2.length()) {
-//                return 0;
-//            } else if (s1.length() < s2.length()) {
-//                return -1;
-//            } else {
-//                return 1;
-//            }
         }
     }
 }
